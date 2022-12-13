b8f49a60ee3d24c6b9a2393e0a64db597877622a
kaushikyelne <kaushik.s.yelne@gmail.com>
2022-12-13 19:32:16 +0530

    conversions

--- 03userinput/main.go
+++ 03userinput/main.go
@@ -4,13 +4,22 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+	"strconv"
+	"strings"
 )
 
 func main() {
 	reader := bufio.NewReader(os.Stdin)
 	fmt.Println("Enter any value: ")
-	// comma ok || err ok
+	// comma ok || err err
 	input, _ := reader.ReadString('\n')
 	fmt.Println("The value is:  ", input)
+	numRating, err := strconv.ParseFloat(strings.TrimSpace(input), 64)
+
+	if err != nil {
+		fmt.Println(err)
+	} else {
+		fmt.Println("Added 1 to your value: ", numRating+1)
+	}
 
 }
